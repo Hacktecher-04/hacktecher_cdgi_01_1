f266b85d5ce1dee2389f2be6b483f2f08207222c
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2025-04-12 13:05:35 +0530

    logout handelled

--- frontend/src/components/landingForm.js
+++ frontend/src/components/landingForm.js
@@ -13,10 +13,10 @@ const landingForm = () => {
                 <div className='w-full h-1/3 flex flex-col items-center justify-center'>
                     <div>
                         <p className='text-xl text-slate-500'>search your recipe here:</p>
-                        <from className='flex items-center mt-1 relative'>
+                        <form className='flex items-center mt-1 relative'>
                             <input type="text" id="txtSearch" placeholder="Jaisa Mood, Vaisa Food..." className='w-[400px]  h-[50px] border rounded px-4 py-2' />
                             <button type='submit' className='w-[40px] h-[40px] absolute pl-1 bg-slate-100 rounded-full flex items-center justify-center right-1 hover:scale-[1.08] cursor-pointer' ><AiOutlineSend size={28} /></button>
-                        </from>
+                        </form>
                     </div>
                 </div>
                 <div className='w-full h-1/3 flex px-15 items-end justify-start'>
